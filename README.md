5b6cd9439d36fc960c2e386a5b2b6206b8ce6de9
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-04-29 10:40:00 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,89 @@
+# Project Summary
+---
+
+In this project, we'll provide practice JavaScript problems to help you better understand `Objects`.
+
+## Setup
+
+* `Fork` this repository.
+* `Clone` your fork.
+* Open `./practice.js` with your code editor.
+* Open `./index.html` with your browser.
+
+## Directions
+
+Complete the first <b>4</b> activities inside of `./practice.js` to make the spec runner pass it's tests. In order to check the progress of the spec runner, open `./SpecRunner.html` with your browser. Remember to commit and push your code often. Good luck!
+
+If you have extra time, try to complete the remaining activites.
+
+## Resources
+
+[MDN Objects](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object)
+
+<details>
+
+<summary> <code> Objects </code></summary>
+
+```js
+// declare an object with properties
+let car = {
+  make: 'Ford',
+  model: 'GT',
+  year: 2019,
+  // This is a method (function) on the object
+  reverse() {
+    return 'Backing Up! Beep! Beep! Beep!'
+  }
+}
+
+// adding or updating properties with dot notation
+car.miles = 100;
+
+// adding or updating properties with bracket notation
+// if miles already exists on the object, this would change it's value
+// if it doesn't this will set it as a key with a value of 150
+car['miles'] = 150
+
+// if a property doesn't exist on an object, it's considered undefined
+
+car.owner === undefined // true
+
+// A function contained in an object is called a method
+// You can add functions to an object just like any other property
+
+car.drive = function() {}
+
+// You can invoke the method by accessing the function (method) on the object
+
+car.reverse(); // 'Backing Up! Beep! Beep! Beep!'
+- or -
+// Though the dot notation version above is more common
+car['reverse'](); // 'Backing Up! Beep! Beep! Beep!'
+```
+
+</details>
+
+
+<details>
+
+<summary> <code> Object Factories </code> </summary>
+
+ ```js
+// Functions that return objects are called object factories
+// They're a blueprint for creating a lot of objects of one type
+
+function createCar(make, model, year) {
+  // the returned object has a property called make whose value is the argument passed to the make parameter above
+  // it's the same for model and year
+  return {
+    make: make,
+    model: model,
+    year: year
+  }
+}
+
+createCar('Ford', 'GT', 2006); // { make: 'Ford', model: 'GT', year: 2006 }
+createCar('Tesla', 'Model S', 2019); // { make: 'Tesla', model: 'Model S', year: 2019 }
+```
+
+</details>
---
+++ test/spec/practiceSpec.js
@@ -0,0 +1,164 @@
+describe('practice.js', function() {
+  describe('me', function() {
+    it('should exist', function() {
+      expect(me).toBeDefined();
+    });
+    it('should be an object', function() {
+      expect(me).toEqual(jasmine.any(Object));
+    });
+    it('should have name and age keys', function() {
+      expect(me.hasOwnProperty('name')).toBe(true);
+      expect(me.hasOwnProperty('age')).toBe(true);
+    });
+    it('should have values for name and age', function() {
+      expect(me.name).toBeTruthy();
+      expect(me.age).toBeTruthy();
+    });
+  });
+
+  describe('favoriteThings', function() {
+    it('should exist', function() {
+      expect(favoriteThings).toBeDefined();
+    });
+    it('should have band, food, person, book, movie, and holiday keys', function() {
+      expect(favoriteThings.hasOwnProperty('band')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('food')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('person')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('book')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('movie')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('holiday')).toBe(true);
+    });
+    it('should have values for all keys', function() {
+      expect(favoriteThings.band).toBeTruthy();
+      expect(favoriteThings.food).toBeTruthy();
+      expect(favoriteThings.person).toBeTruthy();
+      expect(favoriteThings.book).toBeTruthy();
+      expect(favoriteThings.movie).toBeTruthy();
+      expect(favoriteThings.holiday).toBeTruthy();
+    });
+  });
+
+  describe('favoriteThings (part 2)', function() {
+    it('should still exist', function() {
+      expect(favoriteThings).toBeDefined();
+    });
+    it('should now have car and brand keys', function() {
+      expect(favoriteThings.hasOwnProperty('car')).toBe(true);
+      expect(favoriteThings.hasOwnProperty('brand')).toBe(true);
+    });
+    it('should have values for car and brand', function() {
+      expect(favoriteThings.car).toBeTruthy();
+      expect(favoriteThings.brand).toBeTruthy();
+    });
+  });
+
+  describe('favoriteThings (part 3)', function() {
+    it('food should be "Chicken Nuggets"', function() {
+      expect(favoriteThings.food).toBe('Chicken Nuggets');
+    });
+    it('book should be "Harry Potter"', function() {
+      expect(favoriteThings.book).toBe('Harry Potter');
+    });
+  });
+
+  describe('backPack', function() {
+    it('should exist', function() {
+      expect(backPack).toBeDefined();
+    });
+
+    describe('item', function() {
+      it('should exist', function() {
+        expect(item).toBeDefined();
+      });
+      it('should equal "firstPocket"', function() {
+        expect(item).toBe('firstPocket');
+      });
+    });
+
+    it('should have a firstPocket property', function() {
+      // using bracket notation as this is how the readme is written
+      var item = 'firstPocket';
+      expect(backPack.hasOwnProperty(item)).toBe(true);
+    });
+    it('firstPocket should have a value of "chapstick"', function() {
+      expect(backPack.firstPocket).toBe('chapstick');
+    });
+  });
+
+  describe('user2', function() {
+    it('name should be "Aodhan Hayter" and email should be "aodhan@boom.camp" ', function() {
+      expect(user2.name).toBe('Aodhan Hayter');
+      expect(user2.email).toBe('aodhan@boom.camp');
+    });
+  });
+
+  describe('methodCollection', function() {
+    it('should exist', function() {
+      expect(methodCollection).toBeDefined();
+    });
+    describe('alertHello', function() {
+      it('should exist', function() {
+        expect(methodCollection.hasOwnProperty('alertHello')).toBe(true);
+      });
+      it('should be a function', function() {
+        expect(methodCollection.alertHello).toEqual(jasmine.any(Function));
+      });
+      it('should alert "hello"', function() {
+        var alert = spyOn(window, 'alert');
+        methodCollection.alertHello();
+        expect(alert).toHaveBeenCalledWith('hello');
+      });
+    });
+    describe('logHello', function() {
+      it('should exist', function() {
+        expect(methodCollection.hasOwnProperty('logHello')).toBe(true);
+      });
+      it('should be a function', function() {
+        expect(methodCollection.logHello).toEqual(jasmine.any(Function));
+      });
+      it('should console.log "hello"', function() {
+        var consoleLog = spyOn(console, 'log');
+        methodCollection.logHello();
+        expect(consoleLog).toHaveBeenCalledWith('hello');
+      });
+    });
+  });
+
+  describe('makePerson', function() {
+    it('should exist', function() {
+      expect(makePerson).toBeDefined();
+    });
+    it('should return an object', function() {
+      expect(makePerson()).toEqual(jasmine.any(Object));
+    });
+
+    describe('return object', function() {
+      var returnObject = {};
+      beforeEach(function() {
+        returnObject = makePerson('Tester', 'Nov 1', '123456789');
+      });
+      it('should have 3 keys named "name", "birthday" and "ssn"', function() {
+        var numberOfKeys = Object.keys(returnObject).length;
+
+        expect(numberOfKeys).toBe(3);
+        expect(returnObject.hasOwnProperty('name')).toBe(true);
+        expect(returnObject.hasOwnProperty('birthday')).toBe(true);
+        expect(returnObject.hasOwnProperty('ssn')).toBe(true);
+      });
+      it('should assign the values from the arguments passed', function() {
+        expect(returnObject.name).toBe('Tester');
+        expect(returnObject.birthday).toBe('Nov 1');
+        expect(returnObject.ssn).toBe('123456789');
+      });
+    });
+  });
+
+  describe('makeCard', function() {
+    it('should exist', function() {
+      expect(makeCard).toBeDefined();
+    });
+    it('should return an object', function() {
+      expect(makeCard()).toEqual(jasmine.any(Object));
+    });
+  });
+});
