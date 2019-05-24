e53aa0b6c44a799980102f02a362645fc41a7532
Martin Raquion <raquionearl@gmail.com>
2019-05-24 13:27:15 +0800

    Solved Problem 5

--- practice.js
+++ practice.js
@@ -127,13 +127,23 @@ user2.email = 'aodhan@boom.camp';
 
 //Code Here
 
+let methodCollection = {}
+
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
 //Code Here
+methodCollection  = {
+  alertHello(){
+    alert('hello');
+  },
 
+  logHello(){
+    console.log('hello');
+  }
+}
 /*
   Now call your alertHello and logHello methods.
 */
