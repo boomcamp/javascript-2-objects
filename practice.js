8e35b4333c70d0f92aa5eaa3aaa4d8a7491be071
kojiadrianojr <rolando.adriano@boom.camp>
2019-10-18 11:49:14 +0800

    issue#59: Change alert to log

--- practice.js
+++ practice.js
@@ -11,7 +11,7 @@
 /*
   Create an object called me.
   Give it a key of name with the value being your name, and another key of age with the value being your age.
-  Then alert your name using dot notation.
+  Then log your name using dot notation.
 */
 
 //Code here
